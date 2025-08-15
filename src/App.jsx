52301c7414bef312eae4533f62cb46de3d0cbfe4
lexraymond2-lgtm <lexraymond2@gmail.com>
2025-08-14 19:13:35 -1200

    hassh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import Signup from "./components/Signup"
 import Login from "./components/Login"
 import Home from "./components/Home"
@@ -9,17 +9,16 @@ import NotFound from "./components/NotFound"
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/signup" element={<Signup />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/post" element={<Post />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
-
